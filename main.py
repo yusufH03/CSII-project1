ecc79246e4fabc97f6e7a532f59b5d69d669e490
yusufH03 <03eaglesoccer@gmail.com>
2025-12-09 23:41:00 -0600

    completed main() file, as well as reviewed most errors in the logic file

---
+++ main.py
@@ -0,0 +1,18 @@
+#This is the main file that will run the voting app. It imports the logic, creates a window, and executes the app.
+#CSCI-1620: Final Project Part 1
+#Written By: Yusuf Hussain, 12/5/2025
+
+from voting_logic import *
+
+
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.setFixedSize(400, 500)
+    window.show()
+    application.exec()
+
+
+#Run the program
+if __name__ == '__main__':
+    main()
--- voting_logic.py
+++ voting_logic.py
@@ -1,19 +1,48 @@
+#This file contains the logic to run the voting app. It will allow the user to vote for one of 5 candidates on the ballot. In order to do so, the user must enter a valid voter ID, and choose a candidate. They can then view the election results. Another voter can return to the ballot afterward and cast their vote. Votes are tracked in a CSV file, and voter IDs are tracked in a text file. This program imports the auto-generated GUI file, created via Qt Designer.
+#CSCI-1620: Final Project-Part 1
+#Written By: Yusuf Hussain, 12/9/2025
+
+#import everything that is needed (PyQt6, the GUI file, CSV module, and Regular Expressions module)
 from PyQt6.QtWidgets import *
-from gui import Ui_voting_window
+from gui import *
 import csv
 import re
+
 
 class Logic(QMainWindow, Ui_voting_window):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # class variables to store the current candidate being voted for and all the voter IDs
         self.candidate = "candidate"
+        self.id_list = []
+
+        # only allow a ballot to be submitted if a candidate is voted for
+        self.candidate1_checkBox.clicked.connect(lambda: self.enable_submit())
+        self.candidate2_checkBox.clicked.connect(lambda: self.enable_submit())
+        self.candidate3_checkBox.clicked.connect(lambda: self.enable_submit())
+        self.candidate4_checkBox.clicked.connect(lambda: self.enable_submit())
+        self.candidate5_checkBox.clicked.connect(lambda: self.enable_submit())
+        # connect button presses to respective methods
         self.submit_button.clicked.connect(lambda: self.check_ballot())
         self.results_button.clicked.connect(lambda: self.results())
+        self.back_button.clicked.connect(lambda: self.home())
 
+    # allow the user to click submit
+    def enable_submit(self):
+        """
+        Enables submit button
+        :return: None
+        """
+        self.submit_button.setEnabled(True)
 
-    # function to clear and reset ballot
+    # clear and reset ballot
     def clear(self):
+        """
+        Resets the voting window (clears voter ID, candidate checkboxes)
+        :return: None
+        """
         self.id_input.clear()
         self.error_label.setText("")
         self.candidate1_checkBox.setChecked(False)
@@ -22,64 +51,112 @@ class Logic(QMainWindow, Ui_voting_window):
         self.candidate4_checkBox.setChecked(False)
         self.candidate5_checkBox.setChecked(False)
 
+    # checks the validity of the ballot and adds the vote to the data file
+    def check_ballot(self):
+        """
+        This method checks a ballot to see if a candidate has been voted for. It also ensures the voter ID is valid.
+        :return: None
+        """
+        self.clear() # reset the ballot
+        # check who the candidate voted for
 
+        if self.candidate1_checkBox.isChecked():
+            voted_for = 1
+        elif self.candidate2_checkBox.isChecked():
+            voted_for = 2
+        elif self.candidate3_checkBox.isChecked():
+            voted_for = 3
+        elif self.candidate4_checkBox.isChecked():
+            voted_for = 4
+        elif self.candidate5_checkBox.isChecked():
+            voted_for = 5
 
-    def submit(self, vote=None, voter_id=0):
+        # voter id must be unique, numerical, and 9 digits long
+        try:
+            voter_id = self.id_input.text().strip()
+            if voter_id == "":
+                raise ValueError("Enter a 9-digit voter ID")
+            # must be a unique id
+            elif voter_id in self.id_list:
+                raise ValueError("You have already voted")
+            # numbers only
+            elif not voter_id.isdigit():
+                raise ValueError("Please enter a numerical voter ID")
+            # must not be shorter or longer than 9 numbers
+            elif not re.fullmatch('[0-9]{9}', voter_id):
+                raise ValueError(f"{voter_id} is invalid. Please enter a 9-digit voter ID")
+        # display any errors
+        except ValueError as e:
+            self.error_label.setText(f"Error. {e}")
+        else:
+            self.submit(voted_for, voter_id)
+            self.id_list.append(voter_id)
+
+    def submit(self, vote: int = None, voter_id: int = 0):
+        """
+        Method that increments the chosen candidate's vote in a CSV file, and appends the voter ID to a text file.
+        :param vote: number of the candidate that was voted for
+        :param voter_id: voter ID
+        :return: None
+        """
         #candidates on the ballot
         candidates = ["candidate1", "candidate2", "candidate3", "candidate4", "candidate5"]
-        self.candidate = ""
+        #set the candidate that was voted for
+        self.candidate = candidates[vote - 1]
 
-        #read the file
-        with open("data.csv", "r+") as csvfile:
+        # write ID to a text file
+        id_file = open("ids.txt", "a")
+        id_file.write(f"\n{voter_id}")
+        id_file.close()
+
+        # read the file and store it to a 2-D array
+        with open("data.csv", "r") as csvfile:
             reader = csv.reader(csvfile)
-            data = list(reader)
-            #find out & store number of votes for that candidate
-            for row in range(1,len(data)-1):
-                if data[row][0] == self.candidate:
-                    data[row][1] += 1
+            data = []
+            for row in reader:
+                data.append(row)
+
+        # find the right candidate and add 1 to their votes as well as the total.
+        for row in range(2, len(data)): #skip the header
+                if data[row][0] == self.candidate: # find the candidate that was voted for
+                    temp = data[row][1].strip() # temporarily store the current vote as a string
+                    data[row][1] = int(temp)+1 # cast it to an int, add 1, and put the new vote count in data
+                    data[row][1] = f"\t\t\t\t{data[row][1]}" # add back formatting
+                elif data[row][0] == "Total Votes Casted:":
+                    temp = data[row][1] # temporarily store the total
+                    data[row][1] = int(temp)+1 # cast it to an int, add 1, and put the new total in data
                 else:
                     pass
 
-        #overwrite the file with updated vote
-        with open("data.csv", "w") as file:
+        # overwrite the file with updated data
+        with open("data.csv", "w", newline="") as file:
             writer = csv.writer(file)
-            #start and end values
-            start = 0
-            end = 1
-            for row in file:
-                writer.writerow(data[start:end])
-                start = end + 1
-                end += 1
-
-        #################can files be appended in the middle################
-        #with open("data.csv", "w", newline='') as file:
-        #    writer = csv.writer(file)
-        #    #write the header
-        #    writer.writerow(["Candidate", "Number of Votes"])
-        #    for line in file:
-        #        writer.writerow([candidates[vote], ])
-
-
-    #checks the validity of the ballot and adds the vote to the data file
-    def check_ballot(self):
-        #voter id must be 9 digits long and only numerical values
-        try:
-            voter_id = self.id_input.text().strip()
-            if voter_id is None:
-                raise ValueError("Enter a 9-digit voter ID")
-            #voter id must be numbers only
-            elif not voter_id.isdigit():
-                raise ValueError("Please enter a numerical voter ID")
-            #voter id must not be shorter or longer than 9 numbers
-            elif not re.match('^[0-9]{9}', voter_id):
-                raise ValueError(f"{voter_id} is invalid. Please enter a 9-digit voter ID")
-        #display any errors
-        except ValueError as e:
-            self.error_label.setText(f"Error. {e}")
-        self.submit(voter_id)
-
-
+            for row in data:
+                writer.writerow(row)
 
     #display the results of the election
     def results(self):
+        """
+        Displays the results of the election.
+        :return: None
+        """
         self.clear()
+
+        # print the text as it appears in the CSV file
+        with open("data.csv", "r") as file:
+            reader = csv.reader(file)
+            display_text = []
+            for row in reader:
+                display_text.append(" ".join(row)) # add each row to be displayed
+        self.election_results_label.setText("\n".join(display_text))
+
+        self.stackedWidget.setCurrentIndex(1) # go to the results page
+
+    #go back to the voting page from the results
+    def home(self):
+        """
+        Returns to the voting window.
+        :return: None
+        """
+        self.clear()
+        self.stackedWidget.setCurrentIndex(0)
